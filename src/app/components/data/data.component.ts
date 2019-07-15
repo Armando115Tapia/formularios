353214804cdd48d0e3374dffb9691a03e5f5ca0a
Armando Tapia <armandotapia115@hotmail.com>
2019-07-15 17:08:44 -0500

    ejemplo de promesa y validadores asyncronos

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms'
-import { variable } from '@angular/compiler/src/output/output_ast';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms'
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-data',
@@ -15,7 +16,8 @@ export class DataComponent {
        nombre: 'Armando',
        apellido: 'Tapia'
      },
-     correo: 'armandotapia115@hotmail.com'
+     correo: 'armandotapia115@hotmail.com',
+     pasatiempos: ["Correr", "Estudiar", "Muchar"]
    }
 
   constructor() {
@@ -25,25 +27,74 @@ export class DataComponent {
                                                 Validators.required,
                                                 Validators.minLength(5)
         ]),
-                            'apellido': new FormControl('', Validators.required),
+                            'apellido': new FormControl('', [Validators.required, this.noTapia]),
        }),
 
 
 
       'correo': new FormControl('', [
                                     Validators.required,
-                                    Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")])
+                                    Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")]),
+      'pasatiempos': new FormArray([
+        new FormControl('Correr', Validators.required)
+      ]),
+      'username': new FormControl('', Validators.required,this.existeUsuario ),
+      'password1': new FormControl('', Validators.required),
+      'password2': new FormControl()
     });
 
-    this.forma.setValue(this.usuario);
+    // this.forma.setValue(this.usuario);
+    this.forma.controls['password2'].setValidators([
+      Validators.required,
+      this.noIgual.bind(this.forma)
+    ])
 
   }
 
+  agregarPasatiempo(){
+    // para hacer un casteo en ts
+    (<FormArray> this.forma.controls['pasatiempos']).push( new FormControl('Dormir', Validators.required));
+  }
 
+  // este es una validador que no permite que se registre ningun tapia
+
+  noTapia( control: FormControl): {[s:string]:boolean}{
+    if(control.value === "tapia"){
+      return { noTapia: true}
+    }
+    // si retorna null es que no hay problema, se puede registrar
+    return null;
+  }
+
+  // {[s:string]:boolean}
+  noIgual(control: FormControl): any {
+    // console.log('el this con el bind ',this);
+    let forma: any = this;
+    if( control.value !== forma.controls['password1'].value){
+       return {noiguales: true}
+     }
+     return null;
+  }
+
+  existeUsuario(control: FormControl): Promise<any> | Observable<any> {
+      let promesa = new Promise((resolve, reject) => {
+            setTimeout(() => {
+              if(control.value === 'strider'){
+                resolve({existe: true});
+              } else {
+                 resolve(null)
+              }
+            },3000)
+      } );
+      return promesa;
+  }
 
   guardarCambios(){
     console.log("Forma: ",this.forma);
     console.log("Values: ",this.forma.value);
+    // this.forma.reset(this.usuario);
+    // this.forma.reset({nombreCompleto: {nombre: "", apellido:""}, correo:""});
+
 
   }
 
